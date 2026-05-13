0dfbef0d73a2149ff7d8fcde1e438ac33913ae60
Fragoso <joao@fragoso.io>
2026-05-13 11:02:48 +0200

    Update monitor-switch.sh to move workspaces automatically

--- monitor-switch.sh
+++ monitor-switch.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Check if external monitor is connected
+LAPTOP="eDP-1"
+EXTERNAL="" # will be determined dynamically
+
 if hyprctl monitors | grep -q "HDMI-A-1\|DP-1"; then
-    # External monitor connected - disable laptop screen
-    hyprctl keyword monitor "eDP-1,disable"
+  # External monitor connected - disable laptop screen
+  # First, find the active external monitor name
+  EXTERNAL=$(hyprctl monitors | grep -E "Monitor HDMI-A-1|Monitor DP-1" | head -n1 | awk '{print $2}')
+
+  # Move all workspaces from laptop to external monitor
+  for ws in $(hyprctl workspaces | grep -B1 "$LAPTOP" | grep "ID" | awk '{print $3}'); do
+    hyprctl dispatch moveworkspacetomonitor "$ws $EXTERNAL"
+  done
+
+  # Now disable the laptop screen
+  hyprctl keyword monitor "$LAPTOP,disable"
 else
-    # No external monitor - enable laptop screen
-    hyprctl keyword monitor "eDP-1,preferred,auto,1"
+  # No external monitor - enable laptop screen
+  hyprctl keyword monitor "$LAPTOP,preferred,auto,1"
 fi
